a57a675cbe9f0af425e200fd7eb54b5c65ce38d7
ikgoo <lhscj2466@gmail.com>
2023-09-06 00:19:11 +0900

    프로젝트 초기

---
+++ Scenes/World.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+#  https://www.youtube.com/watch?v=mAbG8Oi-SvQ&list=PL9FzW-m48fn2SlrW0KoLT4n5egNdX-W9a&index=1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
