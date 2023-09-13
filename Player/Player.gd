ed6964cbda1313577b332623b5fbea9d42b88104
ikgoo <lhscj2466@gmail.com>
2023-09-13 18:32:53 +0900

    13강

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,16 +1,19 @@
 extends CharacterBody2D
 
-# https://www.youtube.com/watch?v=v75IMavnRUs&list=PL9FzW-m48fn2SlrW0KoLT4n5egNdX-W9a&index=7
 @export var ACCELERATION : float = 500
 @export var MAX_SPEED : float = 80
 @export var FRICTION : float = 750
+@export var ROLL_SPEED : float = 125
 
 @onready var animationPlayer : AnimationPlayer = $AnimationPlayer
 @onready var animationTree : AnimationTree = $AnimationTree
 @onready var animationState = animationTree.get("parameters/playback")
+@onready var hit_box = $Marker2D/HitBox
 
 # 입력 방향
-var inputDirection = Vector2.ZERO
+var inputDirection = Vector2.DOWN
+var lastDirection = Vector2.DOWN
+var rollDirection = Vector2.ZERO
 
 enum PlayerState {
 	MOVE,
@@ -23,27 +26,37 @@ var state : PlayerState = PlayerState.MOVE
 func change_state(chg_state : PlayerState) -> void:
 	state = chg_state
 	if state == PlayerState.ATTACK:
+		animationTree.set("parameters/Attack/blend_position", lastDirection)
 		animationTree.set("parameters/conditions/is_run", false)
 		animationTree.set("parameters/conditions/idle", false)
 		animationTree.set("parameters/conditions/attack", true)
-		velocity = Vector2.ZERO
+		animationTree.set("parameters/conditions/is_roll", false)
 	elif state == PlayerState.MOVE:
-		animationTree.set("parameters/Run/blend_position", inputDirection)
-		animationTree.set("parameters/Idle/blend_position", inputDirection)
-		animationTree.set("parameters/Attack/blend_position", inputDirection)
+		animationTree.set("parameters/Run/blend_position", lastDirection)
 		animationTree.set("parameters/conditions/is_run", true)
 		animationTree.set("parameters/conditions/idle", false)
 		animationTree.set("parameters/conditions/attack", false)
+		animationTree.set("parameters/conditions/is_roll", false)
 	elif state == PlayerState.IDLE:
+		animationTree.set("parameters/Idle/blend_position", lastDirection)
 		animationTree.set("parameters/conditions/is_run", false)
 		animationTree.set("parameters/conditions/idle", true)
 		animationTree.set("parameters/conditions/attack", false)
+		animationTree.set("parameters/conditions/is_roll", false)
+	elif state == PlayerState.ROLL:
+		animationTree.set("parameters/Roll/blend_position", lastDirection)
+		animationTree.set("parameters/conditions/is_run", false)
+		animationTree.set("parameters/conditions/idle", false)
+		animationTree.set("parameters/conditions/attack", false)
+		animationTree.set("parameters/conditions/is_roll", true)
+		
 
 		
 
 
 func _ready():
 	animationTree.active = true
+	hit_box.knockback_vector = lastDirection
 
 
 func _process(delta):
@@ -55,13 +68,20 @@ func _process(delta):
 		PlayerState.ATTACK:
 			attack_state(delta)
 		PlayerState.ROLL:
-			pass
+			roll_state(delta)
 		
 func move_state(delta : float):	
 	inputDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if(inputDirection != Vector2.ZERO):
+		lastDirection = inputDirection
+		hit_box.knockback_vector = lastDirection
+	
 
 	if Input.is_action_pressed("Attack"):
 		change_state(PlayerState.ATTACK)
+	elif Input.is_action_pressed("roll"):
+		rollDirection = lastDirection
+		change_state(PlayerState.ROLL)
 	elif inputDirection != Vector2.ZERO:
 		change_state(PlayerState.MOVE)
 	else:
@@ -73,10 +93,19 @@ func move_state(delta : float):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_slide()
-	
-	
-func attack_state(delta : float):
-	pass
 
+func attack_state(delta : float):
+	velocity = Vector2.ZERO
+
+func roll_state(delta : float):
+	velocity = rollDirection * ROLL_SPEED
+	move_and_slide()
+	
+
+func roll_animation_finished():
+	velocity = velocity * 0.7
+	move_and_slide()
+	change_state(PlayerState.IDLE)	
+	
 func attack_animation_finished():
 	change_state(PlayerState.IDLE)
--- Scenes/World.gd
+++ Scenes/World.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-#  https://www.youtube.com/watch?v=mAbG8Oi-SvQ&list=PL9FzW-m48fn2SlrW0KoLT4n5egNdX-W9a&index=1
+#  https://www.youtube.com/watch?v=mm4D1ETclAc&list=PL9FzW-m48fn2SlrW0KoLT4n5egNdX-W9a&index=13
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
