25b9148c68de9eb9145f9d6000c568563b705212
ikgoo <lhscj2466@gmail.com>
2023-09-19 00:02:19 +0900

    json 저장 로직 구성중

--- FindTheDifference/Resources/DifferenceInfo.gd
+++ FindTheDifference/Resources/DifferenceInfo.gd
@@ -1,2 +1,23 @@
 extends Resource
 
+class_name DifferenceInfo
+
+
+var jsonData : Variant
+var jsonName : String
+@export var mainImage : TextureRect
+
+@export var diff01Image : TextureRect
+@export var diff01 : Vector2
+
+@export var diff02Image : TextureRect
+@export var diff02 : Vector2
+
+@export var diff03Image : TextureRect
+@export var diff03 : Vector2
+
+@export var diff04Image : TextureRect
+@export var diff04 : Vector2
+
+@export var diff05Image : TextureRect
+@export var diff05 : Vector2
