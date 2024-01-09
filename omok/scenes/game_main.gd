56b53ddefbdd48fe31d26a00a03786169f1b45ae
ikgoo <lhscj2466@gmail.com>
2024-01-10 00:06:38 +0900

    오목 시작

---
+++ omok/scenes/game_main.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var rows = 19
+var columns = 19
+var cell_size = 15
+
+var offset = 10
+
+var loc = []
+
+
+#func _draw():
+	#for i in range(rows):
+		#var y = i * cell_size
+		#draw_line(Vector2(0, y), Vector2(columns * cell_size, y), Color.BLACK, 0.5, true)
+#
+	#for j in range(columns):
+		#var x = j * cell_size
+		#draw_line(Vector2(x, 0), Vector2(x, rows * cell_size), Color.BLACK, 0.5, true)
